da039282f08b39f1561e3a1f3ffcde9989a30f20
Mahendra Netam <50073305+netam1010@users.noreply.github.com>
2020-04-17 09:28:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,13 @@
 	<title>php mailer</title>
 </head>
 <body>
-	<div align="center">
-	<h2>`PHP Mailer`</h2> 
+    <h2 align="center">`PHP Mailer`</h2> 
     <form action="mail.php" method="post"> 
     <input type="email" name="email" required autocomplete="off" placeholder="~~enter email~~">
     <br><br>
     <input type="submit" name="submit" value="Submit">
     </form>
-  </div>
+ 
 </body>
 </html>
 <!--Created:17/04/2020
