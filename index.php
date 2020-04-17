747380572c271a81ac6a9328761fb92809110714
Mahendra Netam <mahenranetam02@gmail.com>
2020-04-17 09:03:43 +0530

    added all

---
+++ index.php
@@ -0,0 +1,20 @@
+<!--Created:17/04/2020
+    Developed:Netam1010 -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php mailer</title>
+</head>
+<body>
+	<h2 align="center">`PHP Mailer`</h2>
+	<div align="center">
+    <form action="mail.php" method="post">
+    <input type="email" name="email" required autocomplete="off" placeholder="~~enter email~~">
+    <br><br>
+    <input type="submit" name="submit" value="Submit">
+    </form>
+    </div>
+</body>
+</html>
+<!--Created:17/04/2020
+    Developed:Netam1010 -->
